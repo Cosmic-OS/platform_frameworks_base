d71780536e8dfa9d5da96398305d0ffb2569ae01
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-11-27 14:09:18 -0500

    Port "Option to hide Adb notification icon (2/2)"
    Change-Id: I2a6c5eed646c4a2121890201de2a7b8cca7f1b1a
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6440,6 +6440,17 @@ public final class Settings {
         public static final String DEVICE_PAIRED = "device_paired";
 
         /**
+         * Beginning of Cosmic-OS Secure Settings Additions
+         * @hide
+         */
+
+        /**
+         * Whether to display the ADB notification.
+         * @hide
+         */
+        public static final String ADB_NOTIFY = "adb_notify";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
