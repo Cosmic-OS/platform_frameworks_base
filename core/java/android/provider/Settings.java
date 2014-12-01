fb2af6e006276359ccd573e788dc8cdb56005334
Jon Evans <jon@craftyjon.com>
2014-11-30 20:55:42 -0500

    Port: Wake on plug (1/2)
    abc: set off by default, we have battery led
    use an user system setting instead of global
    
    Change-Id: I5e50a7fd351857d76c1c2fd13c6e737d8902a2f3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4977,6 +4977,13 @@ public final class Settings {
          * @hide
          */
         public static final String SCREENRECORD_QUALITY_MODE = "screenrecord_quality_mode";
+        
+        /**
+         * Whether to wake the display when plugging or unplugging the charger
+         *
+         * @hide
+         */
+        public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
 
         /** End of Cosmic-OS system settings**/
 
