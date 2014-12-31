b5ac42691a96ae35ee295843fe199d4862bdddaa
Matt Mower <mowerm@gmail.com>
2014-12-31 15:02:58 -0600

    fwb: Forward port lockscreen quick unlock [1/2]
    * PasswordTextView QuickUnlock listener functions and method thanks to
      ParanoidAndroid project:
        Keyguard: Implement quick unlock feature [1/2]
        BigBrother1984 <carlosavignano@aospa.co>
        I0b3d92b91f72020d327a7f2f184c2fbc5519a5df

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5744,6 +5744,14 @@ public final class Settings {
                 "statusbar_battery_bar_blend_color_reverse";
 
         /**
+         * Whether to use the custom quick unlock screen control
+         * @hide
+         */
+        public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL =
+                "lockscreen_quick_unlock_control";
+
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
