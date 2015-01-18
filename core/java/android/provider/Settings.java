193c4a19b7c5e9373035c3be054c204cb28d4d8b
Danesh M <daneshm90@gmail.com>
2015-01-18 00:33:00 -0800

    SystemUI: Lockscreen shortcut customization
    Change-Id: I4abf1deed0a342e111453516fa7820191a55ffc4
    
    Revert "SystemUI: recalculate clock position on removing empty shade views"
    
    This reverts commit 023d3365524584de91acf4716822553f38aa507b.
    
    Change-Id: I79bde45cd57e2523dbfa3211c5971c4b82a240c0
    
    Lockscreen : Only animate targets if launched via swipe
    
    If launching via gesture or double tap of power button, showing the
    reveal doesn't make much sense and creates visual jank. Only animate
    for swipes.
    
    Change-Id: I69e1d06c77c12fe39b5ddb01bcc22ed2fff2a9d0
    
    Keyguard : Ensure we expand onIconClicked
    
    Not doing so shows visible clipping when the notification
    panel is expanded mainly due to the limited height of the bar.
    
    Change-Id: I50a35b186b3efb8a0e4ba0ae18ff915aebe236c6
    
    SystemUI: fix double tap power launching custom lockscreen icon
    
    Ticket: RM-232
    
    Change-Id: I2ddf9fce55e9deb9cdf70416f0ed2ee5ce6ce225
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Fix leftAffordanceView visibility bug
    
    When you disable the left bottom lockscreen target, is suppose to be completely disabled like the right target.
    While the icon itself disappears, the preview and hint are still present. This fixes that :-)
    
    See images to see bug:
    http://i.imgur.com/WhBvZkc.jpg & http://i.imgur.com/otyOn6m.png
    
    Change-Id: If5b30f03649d5081409a98dbd36c49b99a5095d7
    
    Ensure camera is launched if doing screen off gesture
    
    Currently it launches whatever the right affordance is set to, which is
    incorrect. Ensure screen off gesture always launches camera.
    
    Change-Id: I251d37ccc85d014ff46714b222b9074b2092f6e1
    
    SystemUI : Ensure previous camera preview is removed
    
    For lockscreen shortcuts, ensure if the camera intent changes,
    we remove the previous view.
    
    Change-Id: I7c44353d25ddc47c3d986acf32dd9a3e85915c82

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4413,6 +4413,44 @@ public final class Settings {
             MOVED_TO_GLOBAL.add(Settings.Global.WIFI_AUTO_CONNECT_TYPE);
         }
 
+        /**
+         * Put a delimited list as a string
+         * @param resolver to access the database with
+         * @param name to store
+         * @param delimiter to split
+         * @param list to join and store
+         * @hide
+         */
+        public static void putListAsDelimitedString(ContentResolver resolver, String name,
+                                                    String delimiter, List<String> list) {
+            String store = TextUtils.join(delimiter, list);
+            putString(resolver, name, store);
+        }
+
+        /**
+         * Get a delimited string returned as a list
+         * @param resolver to access the database with
+         * @param name to store
+         * @param delimiter to split the list with
+         * @return list of strings for a specific Settings.Secure item
+         * @hide
+         */
+        public static List<String> getDelimitedStringAsList(ContentResolver resolver, String name,
+                                                            String delimiter) {
+            String baseString = getString(resolver, name);
+            List<String> list = new ArrayList<String>();
+            if (!TextUtils.isEmpty(baseString)) {
+                final String[] array = TextUtils.split(baseString, Pattern.quote(delimiter));
+                for (String item : array) {
+                    if (TextUtils.isEmpty(item)) {
+                        continue;
+                    }
+                    list.add(item);
+                }
+            }
+            return list;
+        }
+
         /** @hide */
         public static void getMovedToGlobalSettings(Set<String> outKeySet) {
             outKeySet.addAll(MOVED_TO_GLOBAL);
@@ -4957,6 +4995,12 @@ public final class Settings {
          * <p>0 = do not allow use of the package installer
          */
         public static final String INSTALL_NON_MARKET_APPS = "install_non_market_apps";
+
+        /**
+         * Launch actions for left/right lockscreen targets
+         * @hide
+         */
+        public static final String LOCKSCREEN_TARGETS = "lockscreen_target_actions";
 
         /**
          * Comma-separated list of location providers that activities may access. Do not rely on
@@ -8927,6 +8971,44 @@ public final class Settings {
         /** @hide */
         public static void getMovedToSecureSettings(Set<String> outKeySet) {
             outKeySet.addAll(MOVED_TO_SECURE);
+        }
+
+        /**
+         * Put a delimited list as a string
+         * @param resolver to access the database with
+         * @param name to store
+         * @param delimiter to split
+         * @param list to join and store
+         * @hide
+         */
+        public static void putListAsDelimitedString(ContentResolver resolver, String name,
+                                                    String delimiter, List<String> list) {
+            String store = TextUtils.join(delimiter, list);
+            putString(resolver, name, store);
+        }
+
+        /**
+         * Get a delimited string returned as a list
+         * @param resolver to access the database with
+         * @param name to store
+         * @param delimiter to split the list with
+         * @return list of strings for a specific Settings.Secure item
+         * @hide
+         */
+        public static List<String> getDelimitedStringAsList(ContentResolver resolver, String name,
+                                                            String delimiter) {
+            String baseString = getString(resolver, name);
+            List<String> list = new ArrayList<String>();
+            if (!TextUtils.isEmpty(baseString)) {
+                final String[] array = TextUtils.split(baseString, Pattern.quote(delimiter));
+                for (String item : array) {
+                    if (TextUtils.isEmpty(item)) {
+                        continue;
+                    }
+                    list.add(item);
+                }
+            }
+            return list;
         }
 
         /**
