6b3d05331e04f3f4e988b1bada337428966e398b
Konsta <konsta09@gmail.com>
2015-10-24 01:49:00 -0400

    Framework: Volume key cursor control (1/2) This feature is moved to framework so it also works with third party keyboards.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3617,6 +3617,15 @@ public final class Settings {
         public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
 
         /**
+         * Volume keys control cursor in text fields (default is 0)
+         * 0 - Disabled
+         * 1 - Volume up/down moves cursor left/right
+         * 2 - Volume up/down moves cursor right/left
+         * @hide
+         */
+        public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+        /**
          * Whether or not volume button music controls should be enabled to seek media tracks
          * @hide
          */
