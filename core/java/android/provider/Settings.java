964464b4ca2f684e537ee7f86a7c60d7148b769c
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-11-11 16:07:49 -0500

    SystemUI: Double tap to sleep anywhere on the lock screen (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3613,6 +3613,12 @@ public final class Settings {
 	public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
 
         /**
+         * Enable double tap gesture anywhere on the lock screen put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_ANYWHERE = "double_tap_sleep_anywhere";
+
+        /**
          * Enable statusbar double tap gesture on to put device to sleep
          * @hide
          */
