0d23f9210abc10d5daf5d98c6b0bf84765f08d19
Stevespear426 <stephen.k.spear@gmail.com>
2015-12-28 19:04:27 +0100

    FWB: More system animations improvements (1/2)
    Brought back from kitkat by @LorDClockaN
    
    Change-Id: I7974c117c46adb8210d357434912a2cf15fe3204
    Signed-off-by: xlxfoxxlx <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3594,6 +3594,12 @@ public final class Settings {
          */
         public static final String VOLUME_ADJUST_SOUNDS_ENABLED = "volume_adjust_sounds_enabled";
 
+        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
+
+        public static final String ANIMATION_CONTROLS_EXIT_ONLY = "animation_controls_exit_only";
+
+        public static final String ANIMATION_CONTROLS_REVERSE_EXIT = "animation_controls_reverse_exit";
+
         /**  
          * AOKP Custom System Animations
          * @hide
