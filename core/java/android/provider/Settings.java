d7361c7638b9461df96d65763d2eb08abd3b0b65
Alex Cruz <mazdarider23@gmail.com>
2016-02-06 03:59:14 -0500

    Hide Lockscreen clock & date [1/2]
    Before - http://i.imgur.com/XDucx5p.jpg / http://i.imgur.com/GQ4gjn8.png
    After - http://i.imgur.com/m7dnaNB.jpg / http://i.imgur.com/UKjunnW.png
    
    Change-Id: I06b2170f58b002d47f04fdbb5e165ee30cf73157

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3590,6 +3590,18 @@ public final class Settings {
          */
         public static final String IMMERSIVE_RECENTS = "immersive_recents";
 
+        /**
+         * Hide lockscreen date
+         * @hide
+         */
+        public static final String HIDE_LOCKSCREEN_DATE = "hide_lockscreen_date";
+
+        /**
+         * Hide lockscreen clock
+         * @hide
+         */
+        public static final String HIDE_LOCKSCREEN_CLOCK = "hide_lockscreen_clock";
+
        /**
         * USB Data automatic unlock
         */
