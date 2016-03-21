97cd510da40462f309c392c241743cd1c3f2bb3e
George G <kreach3r@users.noreply.github.com>
2016-03-21 14:57:04 +0200

    Allow unlocked USB data access (1/2)
    by @KreAch3R
    
    This bypasses the marshmallow "regression" of blocking and resetting USB data access
    after disconnecting. It also allows saving the default configuration and it survives a reboot.
    Originally, this "feature" was added by Google for security reasons
    so it should be advised to use the below setting under caution.
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3561,6 +3561,11 @@ public final class Settings {
          */
         public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
 
+       /**
+        * USB Data automatic unlock
+        */
+       public static final String USB_DATA_AUTO_UNLOCK = "usb_data_auto_unlock";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
