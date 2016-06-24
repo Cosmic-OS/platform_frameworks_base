cc97bb4c8c2719c3ebc85e68779244776c8f38e3
Alex Cruz <mazdarider23@gmail.com>
2016-06-23 21:31:41 -0400

    Lockscreen Clock Widget Fonts [1/2] Squashed with: *Lockscreen fonts: set correct default value -@BeansTown106 *Update Lockscreen clock font option -@BeansTown106 *fix lockscreen font resetting on reboot -@BeansTown106
    PureNexus Edits:
    *Squashed
    *Modified for use with our observer
    *Cleaned up if statements a bit saved about 15 LOC
    
    AOSiP edit:
    update show clock, date & alarm to new method by beanstown
    
    Change-Id: Id9489275414f5e870aca9aa2465d466b83d649fd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3774,13 +3774,19 @@ public final class Settings {
          * Hide lockscreen date
          * @hide
          */
-        public static final String HIDE_LOCKSCREEN_DATE = "hide_lockscreen_date";
+        public static final String SHOW_LOCKSCREEN_DATE = "show_lockscreen_date";
 
         /**
          * Hide lockscreen clock
          * @hide
          */
-        public static final String HIDE_LOCKSCREEN_CLOCK = "hide_lockscreen_clock";
+        public static final String SHOW_LOCKSCREEN_CLOCK = "show_lockscreen_clock";
+
+        /**
+         * Hide lockscreen date
+         * @hide
+         */
+        public static final String SHOW_LOCKSCREEN_ALARM = "show_lockscreen_alarm";
 
         /**
          * whether to enable or disable vibration on succesful fingerprint auth
@@ -3835,12 +3841,6 @@ public final class Settings {
          * @hide
          */
         public static final String DOZE_SCREEN_BRIGHTNESS = "doze_screen_brightness";
-
-        /**
-         * Hide lockscreen date
-         * @hide
-         */
-        public static final String HIDE_LOCKSCREEN_ALARM = "hide_lockscreen_alarm";
 
        /**
         * USB Data automatic unlock
@@ -3980,6 +3980,12 @@ public final class Settings {
          * @hide
          */
         public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+
+        /**
+         * Change fonts for the system lockscreen clock widget
+         * @hide
+         */
+        public static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
 
         /**
          * Whether or not volume button music controls should be enabled to seek media tracks
