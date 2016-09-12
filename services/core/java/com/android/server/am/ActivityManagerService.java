93e3ae98299370767c8c64d2aa10ed10fd5b26a8
yuanhuihui <yuanhuihui@xiaomi.com>
2016-09-12 17:52:18 +0800

    fix persistent process is killed and restart again and again
    start presistent app  before system booting, in NewProcessRecordLocked
    will set  persistent = true, but not set r.maxAdj = ProcessList.PERSISTENT_PROC_ADJ.
    so this process  may become CACHED_EMPTY, then be killed.
    while persistent = true, will restart again.
    
    https://code.google.com/p/android/issues/detail?id=222224
    
    Change-Id: I91b405ca3f3bbe8934e4a495d9a54eab4f51e21f
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -11753,6 +11753,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                 && userId == UserHandle.USER_SYSTEM
                 && (info.flags & PERSISTENT_MASK) == PERSISTENT_MASK) {
             r.persistent = true;
+            r.maxAdj = ProcessList.PERSISTENT_PROC_ADJ;
         }
         addProcessNameLocked(r);
         return r;
