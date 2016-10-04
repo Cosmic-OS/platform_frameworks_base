fe33f6e65606a89feeb676252d2b353c39eb190d
xyyx <xyyx@mail.ru>
2016-10-04 13:38:38 +0800

    QS: BT easy toggle [1/2]
    Change-Id: I1fadc9b2780c0cc1e912282f44e4f3db909b5e3c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6661,6 +6661,12 @@ public final class Settings {
         public static final String WAKE_GESTURE_ENABLED = "wake_gesture_enabled";
 
         /**
+         * Enable single click to turn Bluetooth on or off.
+         * @hide
+         */
+        public static final String QS_BT_EASY_TOGGLE = "qs_bt_easy_toggle";
+
+        /**
          * Whether to show the keyguard visualizer.
          * Boolean setting. 0 = off, 1 = on.
          * @hide
