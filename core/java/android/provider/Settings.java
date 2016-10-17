7a2fd2e9d2a5776e892c41b4846248949f0e75df
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-10-17 22:00:05 +0800

    SystemUI: Implement battery level text customisations
    3 options - hidden, show beside, or show inside.
    
    Forward port the battery text-related changes from CM 13.0:
    2e0a11bb0bbf22c32f6e15d97ccac10414ec7b44 Forward port battery icon options
    738f5a87a361c9c1027e3764072a37a50467fe6d SystemUI: Expose values in status bar expanded header
    9500061a111fed25bed3ad30173d8e35063f32d0 Themes: Expose hard code for themes in SystemUI
    
    Change-Id: Idec48a3536a3dd529f7dd6076bf33a20cd8020c1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7404,6 +7404,15 @@ public final class Settings {
         public static final String LOCKSCREEN_TARGETS = "lockscreen_target_actions";
 
         /**
+         * Status bar battery %
+         * 0: Hide the battery percentage
+         * 1: Display the battery percentage inside the icon
+         * 2: Display the battery percentage next to the icon
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -7488,7 +7497,8 @@ public final class Settings {
             DOZE_ENABLED,
             DOZE_PULSE_ON_PICK_UP,
             DOZE_PULSE_ON_DOUBLE_TAP,
-            NFC_PAYMENT_DEFAULT_COMPONENT
+            NFC_PAYMENT_DEFAULT_COMPONENT,
+            STATUS_BAR_SHOW_BATTERY_PERCENT
         };
 
         /**
