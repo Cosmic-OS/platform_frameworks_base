BenzoRoms/frameworks_base

Status Bar Logo 2/2
Original idea goes to aicp but I recoded everything and made some improvements
Color for both light and dark modes
Location: Left or Right
Options to show in Status bar and lock screen
Options to hide the logo when a certain number of notification icons are in the status bar

Change-Id: Ib5ebfde2f23f07e79cda71d24526a1f19c80ef7c
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

core/java/android/provider/Settings.java
@@ -4633,6 +4633,57 @@ public final class Settings {
         public static final String SHOW_FOURG = "show_fourg";
 
         /**
+         * Statusbar Benzo logo
+         * @hide
+         */
+        public static final String STATUS_BAR_BENZO_LOGO_SHOW = "status_bar_benzo_logo_show";
+
+        /**
+         * Statusbar Benzo logo on the lock screen
+         * @hide
+         */
+        public static final String STATUS_BAR_BENZO_LOGO_SHOW_ON_LOCK_SCREEN = "status_bar_benzo_logo_show_on_lock_screen";
+
+        /**
+         * Style of Status bar logo
+         * 0 - Left (default)
+         * 1 - Right
+         * @hide
+         */
+        public static final String STATUS_BAR_BENZO_LOGO_STYLE = "status_bar_benzo_logo_style";
+
+        /**
+         * Wether to hide the Benzo logo on the status bar when the custom number of notifications is reached
+         * @hide
+         */
+        public static final String STATUS_BAR_BENZO_LOGO_HIDE_LOGO = "status_bar_benzo_logo_hide_logo";
+
+        /**
+         * Hide the Benzo logo on the status bar when the notification icons will reach this number
+         * 1: 1 notification icon (always)
+         * 2: 2 notification icons
+         * 3: 3 notification icons
+         * 4: 4 notification icons
+         * 5: 5 notification icons
+         * 6: 6 notification icons
+         * default: 1
+         * @hide
+         */
+        public static final String STATUS_BAR_BENZO_LOGO_NUMBER_OF_NOTIFICATION_ICONS = "status_bar_benzo_logo_number_of_notification_icons";
+
+        /**
+         * Statusbar Benzo logo color setting
+         * @hide
+         */
+        public static final String STATUS_BAR_BENZO_LOGO_COLOR = "status_bar_benzo_logo_color";
+
+        /**
+         * Statusbar Benzo logo color in dark mode
+         * @hide
+         */
+        public static final String STATUS_BAR_BENZO_LOGO_COLOR_DARK_MODE = "status_bar_benzo_logo_color_dark_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

core/java/com/android/internal/util/benzo/ColorHelper.java
@@ -1,0 +1,119 @@
+/*
+* Copyright (C) 2015 DarkKat
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.benzo;
+
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+
+import java.util.Random;
+
+public class ColorHelper {
+
+    public static int getBlendColor(int from, int to, float ratio) {
+        final float inverseRatio = 1f - ratio;
+
+        final float a = Color.alpha(to) * ratio + Color.alpha(from) * inverseRatio;
+        final float r = Color.red(to) * ratio + Color.red(from) * inverseRatio;
+        final float g = Color.green(to) * ratio + Color.green(from) * inverseRatio;
+        final float b = Color.blue(to) * ratio + Color.blue(from) * inverseRatio;
+
+        return Color.argb((int) a, (int) r, (int) g, (int) b);
+    }
+
+    public static boolean isColorDark(int color) {
+        double a = 1- (0.299 * Color.red(color)
+                + 0.587 * Color.green(color)
+                + 0.114 * Color.blue(color)) / 255;
+        if (a < 0.5) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static int getLightenOrDarkenColor(int originalColor) {
+        boolean isOriginalColorDark = isColorDark(originalColor);
+        float factor = isOriginalColorDark ? 0.1f : 0.8f;
+        int a = Color.alpha(originalColor);
+        int r = Color.red(originalColor);
+        int g = Color.green(originalColor);
+        int b = Color.blue(originalColor);
+        int newColor;
+
+        if (isOriginalColorDark) {
+            newColor = Color.argb(a,
+                    (int) ((r * (1 - factor) / 255 + factor) * 255),
+                    (int) ((g * (1 - factor) / 255 + factor) * 255),
+                    (int) ((b * (1 - factor) / 255 + factor) * 255));
+        } else {
+            newColor = Color.argb(a,
+                    Math.max((int) (r * factor), 0),
+                    Math.max((int) (g * factor), 0),
+                    Math.max((int) (b * factor), 0));
+        }
+        return newColor;
+    }
+
+    public static String convertToColorHexString(int color, boolean useAlpha) {
+        String alpha = Integer.toHexString(Color.alpha(color));
+        String red = Integer.toHexString(Color.red(color));
+        String green = Integer.toHexString(Color.green(color));
+        String blue = Integer.toHexString(Color.blue(color));
+
+        if (useAlpha) {
+            if (alpha.length() == 1) {
+                alpha = "0" + alpha;
+            }
+        }
+
+        if (red.length() == 1) {
+            red = "0" + red;
+        }
+
+        if (green.length() == 1) {
+            green = "0" + green;
+        }
+
+        if (blue.length() == 1) {
+            blue = "0" + blue;
+        }
+
+        return "#" + (useAlpha ? alpha : "") + red + green + blue;
+    }
+
+    public static int getRandomColor() {
+        Random rand = new Random();
+        return rand.nextInt(0xFFFFFF);
+    }
+
+    public static ColorMatrixColorFilter getColorFilter(int color) {
+        float r = Color.red(color) / 255f;
+        float g = Color.green(color) / 255f;
+        float b = Color.blue(color) / 255f;
+
+        ColorMatrix cm = new ColorMatrix(new float[] {
+                r, 0, 0, 0, 0,
+                0, g, 0, 0, 0,
+                0, 0, b, 0, 0,
+                0, 0, 0, 1, 0,
+        });
+        ColorMatrixColorFilter cf = new ColorMatrixColorFilter(cm);
+        return cf;
+    }
+}
+

core/java/com/android/internal/util/benzo/StatusBarColorHelper.java
@@ -1,0 +1,40 @@
+/*
+* Copyright (C) 2015 DarkKat
+* Copyright (C) 2016 Brett Rogers
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.benzo;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.provider.Settings;
+
+public class StatusBarColorHelper {
+
+    private static final int LIGHT_MODE_COLOR_SINGLE_TONE = 0xffffffff;
+    private static final int DARK_MODE_COLOR_SINGLE_TONE  = 0x99000000;
+    private static final int LIGHT_MODE_ALPHA_SINGLE_TONE = 255;
+    private static final int DARK_MODE_ALPHA_SINGLE_TONE  = 153;
+
+    public static int getLogoColor(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.STATUS_BAR_BENZO_LOGO_COLOR, 0xffffffff);
+    }
+
+    public static int getLogoColorDarkMode(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.STATUS_BAR_BENZO_LOGO_COLOR_DARK_MODE, 0xff000000);
+    }
+}

Cosmic-OS/platform_frameworks_base

core/java/android/provider/Settings.java
@@ -4187,6 +4187,57 @@ public final class Settings {
         public static final String STATUS_BAR_TICKER_FONT_SIZE = "status_bar_ticker_font_size";
 
         /**
+		 * Statusbar Cosmic logo
+		 * @hide
+		 */
+		public static final String STATUS_BAR_COSMIC_LOGO_SHOW = "status_bar_cosmic_logo_show";
+
+        /**
+		 * Statusbar Cosmic logo on the lock screen
+		 * @hide
+		 */
+		public static final String STATUS_BAR_COSMIC_LOGO_SHOW_ON_LOCK_SCREEN = "status_bar_cosmic_logo_show_on_lock_screen";
+
+        /**
+		 * Style of Status bar logo
+         * 0 - Left (default)
+         * 1 - Right
+         * @hide
+		 */
+		public static final String STATUS_BAR_COSMIC_LOGO_STYLE = "status_bar_cosmic_logo_style";
+
+        /**
+         * Wether to hide the Cyanide logo on the status bar when the custom number of notifications is reached
+         * @hide
+         */
+        public static final String STATUS_BAR_COSMIC_LOGO_HIDE_LOGO = "status_bar_cosmic_logo_hide_logo";
+
+        /**
+         * Hide the Cyanide logo on the status bar when the notification icons will reach this number
+         * 1: 1 notification icon (always)
+         * 2: 2 notification icons
+         * 3: 3 notification icons
+         * 4: 4 notification icons
+         * 5: 5 notification icons
+         * 6: 6 notification icons
+         * default: 1
+         * @hide
+         */
+        public static final String STATUS_BAR_COSMIC_LOGO_NUMBER_OF_NOTIFICATION_ICONS = "status_bar_cosmic_logo_number_of_notification_icons";
+
+        /**
+         * Statusbar CyanideL logo color setting
+         * @hide
+         */
+        public static final String STATUS_BAR_COSMIC_LOGO_COLOR = "status_bar_cosmic_logo_color";
+
+        /**
+		 * Statusbar CyanideL logo color in dark mode
+		 * @hide
+		 */
+		public static final String STATUS_BAR_COSMIC_LOGO_COLOR_DARK_MODE = "status_bar_cosmic_logo_color_dark_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

core/java/com/android/internal/util/cosmic/ColorHelper.java
@@ -1,0 +1,119 @@
+/*
+* Copyright (C) 2015 DarkKat
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.cosmic;
+
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+
+import java.util.Random;
+
+public class ColorHelper {
+
+    public static int getBlendColor(int from, int to, float ratio) {
+        final float inverseRatio = 1f - ratio;
+
+        final float a = Color.alpha(to) * ratio + Color.alpha(from) * inverseRatio;
+        final float r = Color.red(to) * ratio + Color.red(from) * inverseRatio;
+        final float g = Color.green(to) * ratio + Color.green(from) * inverseRatio;
+        final float b = Color.blue(to) * ratio + Color.blue(from) * inverseRatio;
+
+        return Color.argb((int) a, (int) r, (int) g, (int) b);
+    }
+
+    public static boolean isColorDark(int color) {
+        double a = 1- (0.299 * Color.red(color)
+                + 0.587 * Color.green(color)
+                + 0.114 * Color.blue(color)) / 255;
+        if (a < 0.5) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static int getLightenOrDarkenColor(int originalColor) {
+        boolean isOriginalColorDark = isColorDark(originalColor);
+        float factor = isOriginalColorDark ? 0.1f : 0.8f;
+        int a = Color.alpha(originalColor);
+        int r = Color.red(originalColor);
+        int g = Color.green(originalColor);
+        int b = Color.blue(originalColor);
+        int newColor;
+
+        if (isOriginalColorDark) {
+            newColor = Color.argb(a,
+                    (int) ((r * (1 - factor) / 255 + factor) * 255),
+                    (int) ((g * (1 - factor) / 255 + factor) * 255),
+                    (int) ((b * (1 - factor) / 255 + factor) * 255));
+        } else {
+            newColor = Color.argb(a,
+                    Math.max((int) (r * factor), 0),
+                    Math.max((int) (g * factor), 0),
+                    Math.max((int) (b * factor), 0));
+        }
+        return newColor;
+    }
+
+    public static String convertToColorHexString(int color, boolean useAlpha) {
+        String alpha = Integer.toHexString(Color.alpha(color));
+        String red = Integer.toHexString(Color.red(color));
+        String green = Integer.toHexString(Color.green(color));
+        String blue = Integer.toHexString(Color.blue(color));
+
+        if (useAlpha) {
+            if (alpha.length() == 1) {
+                alpha = "0" + alpha;
+            }
+        }
+
+        if (red.length() == 1) {
+            red = "0" + red;
+        }
+
+        if (green.length() == 1) {
+            green = "0" + green;
+        }
+
+        if (blue.length() == 1) {
+            blue = "0" + blue;
+        }
+
+        return "#" + (useAlpha ? alpha : "") + red + green + blue;
+    }
+
+    public static int getRandomColor() {
+        Random rand = new Random();
+        return rand.nextInt(0xFFFFFF);
+    }
+
+    public static ColorMatrixColorFilter getColorFilter(int color) {
+        float r = Color.red(color) / 255f;
+        float g = Color.green(color) / 255f;
+        float b = Color.blue(color) / 255f;
+
+        ColorMatrix cm = new ColorMatrix(new float[] {
+                r, 0, 0, 0, 0,
+                0, g, 0, 0, 0,
+                0, 0, b, 0, 0,
+                0, 0, 0, 1, 0,
+        });
+        ColorMatrixColorFilter cf = new ColorMatrixColorFilter(cm);
+        return cf;
+    }
+}
+

core/java/com/android/internal/util/cosmic/StatusBarColorHelper.java
@@ -1,0 +1,40 @@
+/*
+* Copyright (C) 2015 DarkKat
+* Copyright (C) 2016 Brett Rogers
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.cosmic;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.provider.Settings;
+
+public class StatusBarColorHelper {
+
+    private static final int LIGHT_MODE_COLOR_SINGLE_TONE = 0xffffffff;
+    private static final int DARK_MODE_COLOR_SINGLE_TONE  = 0x99000000;
+    private static final int LIGHT_MODE_ALPHA_SINGLE_TONE = 255;
+    private static final int DARK_MODE_ALPHA_SINGLE_TONE  = 153;
+
+    public static int getLogoColor(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.STATUS_BAR_COSMIC_LOGO_COLOR, 0xffffffff);
+    }
+
+    public static int getLogoColorDarkMode(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.STATUS_BAR_COSMIC_LOGO_COLOR_DARK_MODE, 0xff000000);
+    }
+}