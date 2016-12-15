ecc16c0dc917e6a74f73fb857b8f62e00bf8fd44
mydongistiny <jaysonedson@gmail.com>
2016-12-15 14:53:38 -0800

    SystemUI: add toggle for the onehand ui mode [1/2]
    with https://github.com/BenzoRoms/frameworks_base/commit/6eb39f2a8248b44894ad2d657cf8ba17144c93f3
    swiping either left or right on the navbar will enable onehand ui on the side you you swipe towards
    not everyone likes having it on so give them an option to turn it off
    
    Change-Id: I581db476bc8a9acc14594a89114b65950cfda402
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5817,6 +5817,15 @@ public final class Settings {
          * @hide
          */
         public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
+		
+         /**
+         * enable or disable single handed mode
+         * @hide
+         */
+        public static final String ONE_HAND_MODE_ENABLED = "one_hand_mode_enabled";
+
+        /** @hide */
+        private static final Validator ONE_HAND_MODE_ENABLED_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
