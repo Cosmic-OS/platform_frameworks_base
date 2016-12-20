99bd69ca448c9f914c02feb680f8c09fa296002e
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2016-12-19 20:46:11 -0700

    Night Light: automatically set a lower brightness when active [1/2]
    Squashed commit of the following:
    
    commit fe2994aa5f0d0bcd34874cfd7f5775608aafb09e
    Author: ezio84 <brabus84@gmail.com>
    Date:   Thu Dec 15 20:15:30 2016 +0100
    
        Night tile: automatically set a lower brightness when active [1/2]
    
        if enabled, activating the night mode will also set a low
        brightness value, then deactivating the night mode will restore
        the previous user set value.
        Handle a similar low brightness value for both auto and manual
        brightness mode.
    
    commit cd9c52ee42737f92424d7386585a6085e0824713
    Author: ezio84 <brabus84@gmail.com>
    Date:   Thu Dec 15 22:38:53 2016 +0100
    
        Night mode tile brightness: allow to set a different value [1/2]
    
    commit f641e764cfd20979b72f11db91c8ad46b8f2daff
    Author: Ezio Lacandia Bijelkic <brabus84@gmail.com>
    Date:   Mon Dec 19 09:10:35 2016 -0500
    
        Merge the two night light options into one
    
        - Smaller code for the same achievement
    
        https://github.com/DirtyUnicorns/android_frameworks_base/commit/c8c717152afb35e80cabd12a1e2eb1d7ad7d7764
    
        https://github.com/DirtyUnicorns/android_frameworks_base/commit/75d2b44e85efeb1e12e723a14b139fa87d311dc3
    
        Change-Id: I773537d7c32dfeaf020ffd0079c379a775fa1343
    
    commit bd42b0fb730b376235469e87ee792ae1ea251915
    Author: Ezio Lacandia Bijelkic <brabus84@gmail.com>
    Date:   Mon Dec 19 15:54:33 2016 -0500
    
        Wider night light auto low brightness action
    
        - Move the code from qs tile to night display so it can be enabled
        for both the tile and the manual/scheduled night mode
    
        Change-Id: Ic6704b5b3cb56922472f07ced54df464c93c6cd8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6963,6 +6963,12 @@ public final class Settings {
         public static final String DOZE_ENABLED = "doze_enabled";
 
         /**
+         * Low brightness level for the advanced night mode tile
+         * @hide
+         */
+        public static final String QS_NIGHT_BRIGHTNESS_VALUE = "qs_night_brightness_value";
+
+        /**
          * Whether the device should pulse on pick up gesture.
          * @hide
          */
