b0a874ab9c1b3a602eb627f4f2dafd701b9d81b2
ezio84 <brabus84@gmail.com>
2017-01-13 16:25:19 +0100

    QS: Smart Ambient-LiftToWake tile
    Useful when cleaning up the device screen :D
    
    Now that N makes available two separate option for doze notifications
    (display-ambient and Gestures-LiftToWake) we need to handle them both
    with the tile.
    It will act this way:
    1) if any of the two options is enabled, and you tap the tile,
    both options are disabled so you can clean the screen or avoid
    unwanted disturbing wakes;
    2) if you tap the tile again, the tile will restore, separately,
    the two options state you had before disabling them;
    3) if you manually disable the two options, the tile will go to
     disabled state;
    4) if both options (thus the tile) are disabled, and you manually enable
    on of them, the tile will go to enabled state and act again as 1) and 2)
    
    @nathanchance: squashed with the removal of the previous ambient display tile

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -42,7 +42,7 @@ import com.android.systemui.qs.external.TileLifecycleManager;
 import com.android.systemui.qs.external.TileServices;
 import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
-import com.android.systemui.qs.tiles.AmbientDisplayTile;
+import com.android.systemui.qs.tiles.AmbientLiftToWakeTile;
 import com.android.systemui.qs.tiles.AndroidAutoTile;
 import com.android.systemui.qs.tiles.BatteryTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
@@ -448,7 +448,6 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("battery")) return new BatteryTile(this);
         else if (tileSpec.equals("saver")) return new DataSaverTile(this);
         else if (tileSpec.equals("night")) return new NightDisplayTile(this);
-        else if (tileSpec.equals("ambient_display")) return new AmbientDisplayTile(this);
         else if (tileSpec.equals("adb_network")) return new AdbOverNetworkTile(this);
         else if (tileSpec.equals("brightness")) return new BrightnessTile(this);
         else if (tileSpec.equals("caffeine")) return new CaffeineTile(this);
@@ -463,6 +462,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("navigation_bar")) return new NavigationBarTile(this);
         else if (tileSpec.equals("expanded_desktop")) return new ExpandedDesktopTile(this);
         else if (tileSpec.equals("androidauto")) return new AndroidAutoTile(this);
+        else if (tileSpec.equals("ambient")) return new AmbientLiftToWakeTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
