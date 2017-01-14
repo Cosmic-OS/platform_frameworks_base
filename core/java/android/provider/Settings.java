aa14fb52be2b8aa5635baf9a02ae5a91c78e0368
ezio84 <brabus84@gmail.com>
2017-01-14 18:29:15 +0100

    DUI: Smartbar: double tap to sleep [1/3]
    This is more reliable than double tap to sleep action on the buttons themselves

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4405,6 +4405,12 @@ public final class Settings {
         public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
 
         /**
+         *  Enable smartbar double tap to sleep gesture
+         * @hide
+         */
+        public static final String SMARTBAR_DOUBLETAP_SLEEP = "smartbar_doubletap_sleep";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
