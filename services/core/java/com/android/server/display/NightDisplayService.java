3686ed10fc41bda7b240b5d0030af63862038b17
Sagar Shah <sagarshah2326@gmail.com>
2017-02-20 17:39:53 +0100

    Revert "Fix manual brightness going to 0 after reboot"
    This reverts commit 812fb06e1a7c57bfd93ac2f3238cda7e32a9ec24.

--- services/core/java/com/android/server/display/NightDisplayService.java
+++ services/core/java/com/android/server/display/NightDisplayService.java
@@ -272,8 +272,6 @@ public final class NightDisplayService extends SystemService
         if (mIsActivated == null || mIsActivated != activated) {
             Slog.i(TAG, activated ? "Turning on night display" : "Turning off night display");
 
-            Boolean isReboot = mIsActivated;
-
             if (mAutoMode != null) {
                 mAutoMode.onActivated(activated);
             }
@@ -328,9 +326,7 @@ public final class NightDisplayService extends SystemService
                 }
             });
             mColorMatrixAnimator.start();
-            if (isReboot != null) {
-                setBrightness(mIsActivated);
-            }
+            setBrightness(mIsActivated);
         }
     }
 
