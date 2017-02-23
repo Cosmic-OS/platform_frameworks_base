db4af91e1ac8fa499885ff1aac74b516c64e815c
Sagar Shah <sagarshah2326@gmail.com>
2017-02-23 16:24:23 +0100

    Revert "circlebattery: bring back pulsing animation"
    This reverts commit e266a49c83e4968bc9b3cd839f3dbd8aaa364fb0.

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -16,10 +16,7 @@
 
 package com.android.systemui;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.animation.ArgbEvaluator;
-import android.animation.ValueAnimator;
 import android.annotation.Nullable;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -156,7 +153,6 @@ public class BatteryMeterDrawable extends Drawable implements
     private Drawable mFrameDrawable;
     private StopMotionVectorDrawable mLevelDrawable;
     private Drawable mBoltDrawable;
-    private ValueAnimator mAnimator;
 
     private int mTextGravity;
 
@@ -306,11 +302,6 @@ public class BatteryMeterDrawable extends Drawable implements
         mLevel = level;
         mPluggedIn = pluggedIn;
         mCharging = charging;
-
-        if (mStyle == BATTERY_STYLE_CIRCLE) {
-            animateCircleBattery(level, pluggedIn, charging);
-        }
-
         postInvalidate();
     }
 
@@ -452,37 +443,6 @@ public class BatteryMeterDrawable extends Drawable implements
                 }
             }
             return color;
-        }
-    }
-
-    public void animateCircleBattery(int level, boolean pluggedIn, boolean charging) {
-        if (charging) {
-            if (mAnimator != null) mAnimator.cancel();
-
-            final int defaultAlpha = mLevelDrawable.getAlpha();
-            mAnimator = ValueAnimator.ofInt(defaultAlpha, 0, defaultAlpha);
-            mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    mLevelDrawable.setAlpha((int) animation.getAnimatedValue());
-                    invalidateSelf();
-                }
-            });
-            mAnimator.addListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationCancel(Animator animation) {
-                    mLevelDrawable.setAlpha(defaultAlpha);
-                    mAnimator = null;
-                }
-
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLevelDrawable.setAlpha(defaultAlpha);
-                    mAnimator = null;
-                }
-            });
-            mAnimator.setDuration(2000);
-            mAnimator.start();
         }
     }
 
