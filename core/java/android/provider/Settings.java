ff38c00a3031c920faf3417d0784ae79b277f30c
xyyx <xyyx@mail.ru>
2017-09-11 10:16:44 +0800

    Allow to disable HOME key when ringing [1/2] In early versions Google disabled it by default. In 7.1.x HOME key always works that is very annoying for users

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4210,6 +4210,12 @@ public final class Settings {
         public static final String BATTERY_SAVER_MODE_COLOR = "battery_save_mode_color";
 
         /**
+         * Whether the Home button works during call
+         * @hide
+         */
+        public static final String ALLOW_INCALL_HOME = "allow_incall_home";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
