9297a74dc8d813d7a60218270cc2ae9367c45ee0
maxwen <max.weninger@gmail.com>
2017-10-12 20:24:01 +0200

    FWB: add status bar logo (1/2)
    Change-Id: I86eb407557197229481940e565ab711eb56312b0
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4488,6 +4488,11 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_TICKER = "status_bar_show_ticker";
 
         /**
+         * @hide
+         */
+        public static final String STATUS_BAR_LOGO = "status_bar_logo";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4667,6 +4672,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_DATE_DISPLAY);
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_DATE_STYLE);
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_DATE_FORMAT);
+            PRIVATE_SETTINGS.add(STATUS_BAR_LOGO);
         }
 
         /**
