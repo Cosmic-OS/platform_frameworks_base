5f22fe1fd97d781bd9970c997d699f975d80269a
DennySPB <dennyspb@gmail.com>
2017-11-30 12:27:34 +0300

    Add haptic on action handle
    Change-Id: Id33b7eecc679bf2755172628d228375219265a68

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4340,6 +4340,12 @@ public final class Settings {
          */
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
+	 /**
+         * Whether the haptic feedback on action instead of touch
+         * @hide
+         */
+        public static final String HAPTIC_ON_ACTION_KEY = "haptic_on_action_key";
+
         /**
          * Applications list where heasdup should't show
          *
