b859753dfab89173fce941e52895106abddbcd14
Alex Cruz <mazdarider23@gmail.com>
2018-01-22 19:48:15 +1100

    Show 4G instead of LTE [1/2] - Added to statusbar items under the AOSP tuner instead of 2 parts - Added a category to organize things a bit - Added SettingsObserver in PSB - Hide the switch on WiFi-Only devices (thanks AICP for the idea) - Removed config that was used in overlays
    RR edits:
    Yes i hate the tuner prefs .im gonna move this switch to settings
    - Varun
    
    Change-Id: I1643b9d34b6c2d2081d72288d7daf094fff20361
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4539,6 +4539,12 @@ public final class Settings {
         public static final String RECENTS_ICON_PACK = "recents_icon_pack";
 
         /**
+         * Whether to display the 4g or lte network icon in the statusbar
+         * @hide
+         */
+        public static final String SHOW_LTE_FOURGEE = "show_lte_fourgee";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
