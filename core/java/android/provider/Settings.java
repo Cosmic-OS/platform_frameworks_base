c2b1fa9e613eb3d67bcc9c2a22c913084c5aba97
maxwen <max.weninger@gmail.com>
2018-01-25 15:03:47 +0100

    Screenrecord tile: save quality level
    save state set in qs tile into settings
    
    Change-Id: I708b324a22c3e605fa6411c1a9e1de28b584938b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4464,6 +4464,11 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_DATE_FORMAT = "statusbar_clock_date_format";
 
         /**
+         * @hide
+         */
+        public static final String SCREENRECORD_QUALITY_MODE = "screenrecord_quality_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
