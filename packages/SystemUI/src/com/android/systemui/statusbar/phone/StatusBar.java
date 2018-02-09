a1b903322a9da76c8758916e494700633eb8b9dd
ManoloRey <cleitonmca@gmail.com>
2018-02-09 19:33:23 +0000

    Fixes

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -671,7 +671,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             super.onMetadataChanged(metadata);
             if (DEBUG_MEDIA) Log.v(TAG, "DEBUG_MEDIA: onMetadataChanged: " + metadata);
             mMediaMetadata = metadata;
-            updateMediaMetaData(true, true)
+            updateMediaMetaData(true, true);
 
             if (mNavigationBar != null) {
                 setMediaPlaying();
@@ -728,6 +728,9 @@ public class StatusBar extends SystemUI implements DemoMode,
                     tick(entry.notification, true, true, mMediaMetadata);
                     break;
                 }
+            }
+        }
+    }
 
     private void tickTrackInfo(MediaController mc) {
         ArrayList<Entry> activeNotifications = mNotificationData.getAllNotifications();
