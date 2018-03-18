868030d5fc99b9b866506edce9cca60985cae66a
George G <kreach3r@users.noreply.github.com>
2018-03-18 23:49:28 +0200

    Correct colors for Pixel navbar icons
    Dark icons were never supposed to pull from framework's black color, fix that.
    Also revert dcd381b21b1d ("OpaLayout: Evaluate intensity correctly").
    Now the behavior is identical to a stock Pixel image
    
    Change-Id: I972220f7a790fec03ba086a2b6ce9c5851c41a09
    Fixes: 4c450fea302d ("Add Pixel navbar icons")

--- packages/SystemUI/src/com/google/android/systemui/OpaLayout.java
+++ packages/SystemUI/src/com/google/android/systemui/OpaLayout.java
@@ -98,8 +98,8 @@ public class OpaLayout extends FrameLayout implements ButtonInterface{
 
     public OpaLayout(Context context) {
         super(context);
-        mDarkModeFillColor = context.getColor(android.R.color.black);
-        mLightModeFillColor = context.getColor(android.R.color.white);
+        mDarkModeFillColor = context.getColor(R.color.dark_mode_icon_color_single_tone);
+        mLightModeFillColor = context.getColor(R.color.light_mode_icon_color_single_tone);
         mFastOutSlowInInterpolator = Interpolators.FAST_OUT_SLOW_IN;
         mHomeDisappearInterpolator = new PathInterpolator(0.8f, 0f, 1f, 1f);
         mCollapseInterpolator = Interpolators.FAST_OUT_LINEAR_IN;
@@ -128,8 +128,8 @@ public class OpaLayout extends FrameLayout implements ButtonInterface{
 
     public OpaLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mDarkModeFillColor = context.getColor(android.R.color.black);
-        mLightModeFillColor = context.getColor(android.R.color.white);
+        mDarkModeFillColor = context.getColor(R.color.dark_mode_icon_color_single_tone);
+        mLightModeFillColor = context.getColor(R.color.light_mode_icon_color_single_tone);
         mFastOutSlowInInterpolator = Interpolators.FAST_OUT_SLOW_IN;
         mHomeDisappearInterpolator = new PathInterpolator(0.8f, 0f, 1f, 1f);
         mCollapseInterpolator = Interpolators.FAST_OUT_LINEAR_IN;
@@ -158,8 +158,8 @@ public class OpaLayout extends FrameLayout implements ButtonInterface{
 
     public OpaLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        mDarkModeFillColor = context.getColor(android.R.color.black);
-        mLightModeFillColor = context.getColor(android.R.color.white);
+        mDarkModeFillColor = context.getColor(R.color.dark_mode_icon_color_single_tone);
+        mLightModeFillColor = context.getColor(R.color.light_mode_icon_color_single_tone);
         mFastOutSlowInInterpolator = Interpolators.FAST_OUT_SLOW_IN;
         mHomeDisappearInterpolator = new PathInterpolator(0.8f, 0f, 1f, 1f);
         mCollapseInterpolator = Interpolators.FAST_OUT_LINEAR_IN;
@@ -188,8 +188,8 @@ public class OpaLayout extends FrameLayout implements ButtonInterface{
 
     public OpaLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        mDarkModeFillColor = context.getColor(android.R.color.black);
-        mLightModeFillColor = context.getColor(android.R.color.white);
+        mDarkModeFillColor = context.getColor(R.color.dark_mode_icon_color_single_tone);
+        mLightModeFillColor = context.getColor(R.color.light_mode_icon_color_single_tone);
         mFastOutSlowInInterpolator = Interpolators.FAST_OUT_SLOW_IN;
         mHomeDisappearInterpolator = new PathInterpolator(0.8f, 0f, 1f, 1f);
         mCollapseInterpolator = Interpolators.FAST_OUT_LINEAR_IN;
