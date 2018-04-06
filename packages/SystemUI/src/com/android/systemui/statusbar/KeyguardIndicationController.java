67eb7bbb874187f9b076c33617b8eae762dbaa48
Alexander Martinz <alex@amartinz.at>
2018-04-06 04:25:53 -0400

    Add support for OEM fast chargers
    Based on AOSPA fw/base 0af31949c69f7c306b49ecf2c25579300973fdc9
      - https://github.com/AOSPA/android_frameworks_base/commit/0af31949c69f7c306b49ecf2c25579300973fdc9
    
    Allows to indicate, whether a device is charged using a proprietary oem
    fast charge solution, which write their status to sysfs.
    
    The oem fast charge detection tries to be as generic as possible and is
    configured via overlays.
    
    @Sirhc95: Keep it inline with dash charging support as well as adding
    a custom keyguard indicator when the sysfs path is defined.
    
    For generic aosp charger detection, indication will remain as "Charging rapidly".
    
    @sagarshah1729: Make it compatible with turbo charging as well
    
    Change-Id: I7d380611428607d415dd7cd6964bb86a123e019a
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -371,6 +371,11 @@ public class KeyguardIndicationController {
                         ? R.string.keyguard_indication_turbo_power_time
                         : R.string.keyguard_plugged_in_turbo_power_charging;
                 break;
+            case KeyguardUpdateMonitor.BatteryStatus.CHARGING_OEM_FAST:
+                chargingId = hasChargingTime
+                        ? R.string.keyguard_indication_fast_charging_time
+                        : R.string.keyguard_plugged_in_fast_charging;
+                break;
             case KeyguardUpdateMonitor.BatteryStatus.CHARGING_SLOWLY:
                 chargingId = hasChargingTime
                         ? R.string.keyguard_indication_charging_time_slowly
