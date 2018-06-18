AICP/frameworks_base

FWB: Statusbar logo position (1/2)
Change-Id: Ifee67ebd567f6121f826f4899572e2040e4943fb

packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -64,7 +64,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     // Aicp additions start
     private View mAicpLogo;
-    private boolean mShowLogo;
+    private View mAicpLogoRight;
+    private int mShowLogo;
     private final Handler mHandler = new Handler();
 
     private class AicpSettingsObserver extends ContentObserver {
@@ -120,6 +121,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mSignalClusterView = mStatusBar.findViewById(R.id.signal_cluster);
         Dependency.get(DarkIconDispatcher.class).addDarkReceiver(mSignalClusterView);
         mAicpLogo = mStatusBar.findViewById(R.id.status_bar_logo);
+        mAicpLogoRight = mStatusBar.findViewById(R.id.status_bar_logo_right);
         updateSettings(false);
         // Default to showing until we know otherwise.
         showSystemIconArea(false);
@@ -214,22 +216,28 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     public void hideSystemIconArea(boolean animate) {
         animateHide(mSystemIconArea, animate, true);
+        if (mShowLogo == 2) {
+            animateHide(mAicpLogoRight, animate, false);
+        }
     }
 
     public void showSystemIconArea(boolean animate) {
         animateShow(mSystemIconArea, animate);
+        if (mShowLogo == 2) {
+            animateShow(mAicpLogoRight, animate);
+        }
     }
 
     public void hideNotificationIconArea(boolean animate) {
         animateHide(mNotificationIconAreaInner, animate, true);
-        if (mShowLogo) {
-            animateHide(mAicpLogo, animate, true);
+        if (mShowLogo == 1) {
+            animateHide(mAicpLogo, animate, false);
         }
     }
 
     public void showNotificationIconArea(boolean animate) {
         animateShow(mNotificationIconAreaInner, animate);
-        if (mShowLogo) {
+        if (mShowLogo == 1) {
             animateShow(mAicpLogo, animate);
         }
     }
@@ -299,14 +307,23 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     public void updateSettings(boolean animate) {
         mShowLogo = Settings.System.getIntForUser(
                 getContext().getContentResolver(), Settings.System.STATUS_BAR_LOGO, 0,
-                UserHandle.USER_CURRENT) == 1;
+                UserHandle.USER_CURRENT);
         if (mNotificationIconAreaInner != null) {
-            if (mShowLogo) {
+            if (mShowLogo == 1) {
                 if (mNotificationIconAreaInner.getVisibility() == View.VISIBLE) {
                     animateShow(mAicpLogo, animate);
                 }
-            } else {
+            } else if (mShowLogo != 1) {
                 animateHide(mAicpLogo, animate, false);
+            }
+        }
+        if (mSystemIconArea != null) {
+            if (mShowLogo == 2) {
+                if (mSystemIconArea.getVisibility() == View.VISIBLE) {
+                    animateShow(mAicpLogoRight, animate);
+                }
+            } else if (mShowLogo != 2) {
+                animateHide(mAicpLogoRight, animate, false);
             }
         }
     }

Cosmic-OS/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -77,7 +77,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     // COSMIC Status Logo
     private View mCOSMICLogo;
-    private boolean mShowLogo;
+    private View mCOSMICLogoRight;
+    private int mShowLogo;
     private final Handler mHandler = new Handler();
 
     private class COSMICSettingsObserver extends ContentObserver {
@@ -163,6 +164,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mCenterClockLayout = (LinearLayout) mStatusBar.findViewById(R.id.center_clock_layout);
         Dependency.get(DarkIconDispatcher.class).addDarkReceiver(mSignalClusterView);
         mCOSMICLogo = mStatusBar.findViewById(R.id.status_bar_logo);
+        mCOSMICLogoRight = mStatusBar.findViewById(R.id.status_bar_logo_right);
         updateSettings(false);
         // Default to showing until we know otherwise.
         showSystemIconArea(false);
@@ -269,25 +271,31 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     public void hideSystemIconArea(boolean animate) {
         animateHide(mSystemIconArea, animate, true);
         animateHide(mCenterClockLayout, animate);
+        if (mShowLogo == 2) {
+            animateHide(mCOSMICLogoRight, animate, false);
+        }
     }
 
     public void showSystemIconArea(boolean animate) {
         animateShow(mSystemIconArea, animate);
         animateShow(mCenterClockLayout, animate);
+        if (mShowLogo == 2) {
+            animateShow(mCOSMICLogoRight, animate);
+        }
     }
 
     public void hideNotificationIconArea(boolean animate) {
         animateHide(mNotificationIconAreaInner, animate, true);
         animateHide(mCenterClockLayout, animate);
-        if (mShowLogo) {
-            animateHide(mCOSMICLogo, animate, true);
+        if (mShowLogo == 1) {
+            animateHide(mCOSMICLogo, animate, false);
         }
     }
 
     public void showNotificationIconArea(boolean animate) {
         animateShow(mNotificationIconAreaInner, animate);
         animateShow(mCenterClockLayout, animate);
-        if (mShowLogo) {
+        if (mShowLogo == 1) {
             animateShow(mCOSMICLogo, animate);
         }
     }
@@ -372,14 +380,23 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     public void updateSettings(boolean animate) {
         mShowLogo = Settings.System.getIntForUser(
                 getContext().getContentResolver(), Settings.System.STATUS_BAR_LOGO, 0,
-                UserHandle.USER_CURRENT) == 1;
+                UserHandle.USER_CURRENT);
         if (mNotificationIconAreaInner != null) {
-            if (mShowLogo) {
+            if (mShowLogo == 1) {
                 if (mNotificationIconAreaInner.getVisibility() == View.VISIBLE) {
                     animateShow(mCOSMICLogo, animate);
                 }
-            } else {
+            } else if (mShowLogo != 1) {
                 animateHide(mCOSMICLogo, animate, false);
+            }
+        }
+        if (mSystemIconArea != null) {
+            if (mShowLogo == 2) {
+                if (mSystemIconArea.getVisibility() == View.VISIBLE) {
+                    animateShow(mCOSMICLogoRight, animate);
+                }
+            } else if (mShowLogo != 2) {
+                animateHide(mCOSMICLogoRight, animate, false);
             }
         }
     }