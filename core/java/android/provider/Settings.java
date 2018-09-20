3f3998f695c8a035ca6c4886411be877fff85b94
SuperDroidBond <superdroidbond@yahoo.com>
2018-09-20 15:10:37 +0530

    Statusbar Clock Font styles [1/2]
    Add Clock size & font style.
    
    Port to Pie - SuperDroidBond
    Clock fonts and size - @temasek
    Port to Oreo by @varund7726

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5866,6 +5866,18 @@ public final class Settings {
         public static final String STATUS_BAR_CARRIER_FONT_STYLE = "status_bar_carrier_font_style";
 
         /**
+         * Clock font size
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_SIZE = "status_bar_clock_size";
+
+        /**
+         * Clock font style
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_FONT_STYLE = "status_bar_clock_font_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
