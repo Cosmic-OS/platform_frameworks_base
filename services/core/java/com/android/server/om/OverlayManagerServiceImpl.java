2f99ba03f624f9d0831bd6f9f7ab5f47e7375aba
Kshitij Gupta <kshitijgm@gmail.com>
2019-04-27 17:33:44 +0530

    base: Introduce RGB accenter [1/2]
    Change-Id: I0a6917b417eae87d80decd4e16a85295f32a0cd6
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
+++ services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
@@ -742,6 +742,10 @@ final class OverlayManagerServiceImpl {
         mIdmapManager.removeIdmap(oi, oi.userId);
     }
 
+    public void reloadAssets(final String packageName, final int userId) {
+        mListener.onOverlaysChanged(packageName, userId);
+    }
+
     interface OverlayChangeListener {
 
         /**
