0caa0b0d909660d59858d9cbe1a844169621067f
Pranav Vashi <neobuddy89@gmail.com>
2019-07-06 20:40:36 +0530

    GamingMode: Add tile
    * Also clean up leftover code.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I5e1b719fd550608a55a3e2dd80b6401e2c287011
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Sebastian (Felvesthe) <felvesthe@outlook.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -39,6 +39,7 @@ import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
+import com.android.systemui.qs.tiles.GamingModeTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.ImmersiveTile;
 import com.android.systemui.qs.tiles.IntentTile;
@@ -158,6 +159,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new OneHandTile(mHost);
             case "cpuinfo":
                 return new CPUInfoTile(mHost);
+            case "gaming":
+                return new GamingModeTile(mHost);
         }
 
         // Intent tiles.
