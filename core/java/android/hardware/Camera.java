fcddde3e145ce66481f99ceb6db1e82b5abb01f6
Sagar Shah <sagarshah2326@gmail.com>
2019-12-08 08:18:26 +0100

    fwb: camera: Add missing import
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -58,6 +58,7 @@ import com.android.internal.app.IAppOpsService;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 
