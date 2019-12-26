afa43e53576abdcac9852d0f4b5a4a9b9210221e
ezio84 <brabus84@gmail.com>
2019-12-26 23:34:28 +0100

    Ambient instead of losckreen on wake gesture: code fixes
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -149,9 +149,7 @@ public class DozeTriggers implements DozeMachine.Part {
 
         if (isWakeDisplay) {
             onWakeScreen(wakeEvent, mMachine.isExecutingTransition() ? null : mMachine.getState());
-        } else if (isLongPress
-                // whether to show ambient or lockscreen if AoD is disabled and we do a wake gesture like lift to wake or double tap
-                || (!mConfig.alwaysOnEnabled(UserHandle.USER_CURRENT) && mConfig.isAmbientGestureEnabled(UserHandle.USER_CURRENT))) {
+        } else if (isLongPress) {
             requestPulse(pulseReason, sensorPerformedProxCheck);
         } else if (isWakeLockScreen) {
             if (wakeEvent) {
@@ -187,6 +185,10 @@ public class DozeTriggers implements DozeMachine.Part {
     }
 
     private void gentleWakeUp(int reason) {
+        if (!mConfig.alwaysOnEnabled(UserHandle.USER_CURRENT) && mConfig.isAmbientGestureEnabled(UserHandle.USER_CURRENT)) {
+            requestPulse(reason, true /* alreadyPerformedProxCheck */);
+            return;
+        }
         // Log screen wake up reason (lift/pickup, tap, double-tap)
         mMetricsLogger.write(new LogMaker(MetricsEvent.DOZING)
                 .setType(MetricsEvent.TYPE_UPDATE)
